ef9991fab92a1b4c97866007f9dde27b9241bab9
SHARMA JATIN <sharmajatin392004@gmail.com>
2025-07-29 20:47:50 +0530

    Fix: Corrected icon paths and checked case sensitivity for deployment

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,8 +11,8 @@ const Sidebar = () => {
       >
         {/* Profile Section */}
         <div className="profile flex justify-between items-center h-12 p-3 mt-1 rounded-lg hover:bg-[#4C4E54]">
-          <img src="/Assets/User-Picture.svg" alt="User" className="h-6 w-6" />
-          <img src="/Assets/Icon-Ellipsis.svg" alt="Menu" className="h-6 w-6" />
+          <img src="/assets/User-Picture.svg" alt="User" className="h-6 w-6" />
+          <img src="/assets/Icon-Ellipsis.svg" alt="Menu" className="h-6 w-6" />
         </div>
 
         {/* Main Navigation */}
@@ -30,7 +30,7 @@ const Sidebar = () => {
           <div className="text-xs text-[#9898A6] font-medium mb-1">MY COLLECTIONS</div>
           <Link to="/playlists">
             <div className="flex items-center gap-2 px-3 py-2 mt-2 rounded-lg hover:bg-[#4c4e54]">
-              <img src="/Assets/Icon-Playlists.svg" alt="Playlists Icon" />
+              <img src="/assets/Icon-Playlists.svg" alt="Playlists Icon" />
               <label className="cursor-pointer">Playlists</label>
             </div>
           </Link>
@@ -40,15 +40,15 @@ const Sidebar = () => {
       {/* Mobile Bottom Navigation */}
       <div className="fixed bottom-0 z-50 w-full bg-[#212124] flex justify-around items-center p-2 md:hidden">
         <Link to="/" className="flex flex-col items-center justify-center">
-          <img src="/Assets/house.svg" alt="Home" className="h-6 w-6" />
+          <img src="/assets/house.svg" alt="Home" className="h-6 w-6" />
           <div className="text-xs leading-6 mt-1">Home</div>
         </Link>
         <Link to="/explore" className="flex flex-col items-center justify-center">
-          <img src="/Assets/Search.svg" alt="Queue" className="h-6 w-6" />
+          <img src="/assets/Search.svg" alt="Queue" className="h-6 w-6" />
           <div className="text-xs leading-6 mt-1">Explore</div>
         </Link>
         <Link to="/playlists" className="flex flex-col items-center justify-center">
-          <img src="/Assets/list-music.svg" alt="Playlist" className="h-6 w-6" />
+          <img src="/assets/list-music.svg" alt="Playlist" className="h-6 w-6" />
           <div className="text-xs leading-6 mt-1">Playlists</div>
         </Link>
       </div>
